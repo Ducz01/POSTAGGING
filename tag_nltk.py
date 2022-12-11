344953997a8fa8b859eb5349ab0635de6192cc83
Ducz01 <tranthinhduc01@web.de>
2022-12-11 15:36:35 +0100

    add index and time for nltk

--- tag_nltk.py
+++ tag_nltk.py
@@ -3,6 +3,7 @@ from nltk.tokenize import word_tokenize
 from os import listdir
 from os.path import isfile, join
 import os
+import time
 
 dataset_dir = 'iwv_dataset_parsed/'
 leichte_sprache_dataset = os.path.join(dataset_dir, "markdown_leichte_sprache/")
@@ -14,16 +15,27 @@ tagged_standard = join(tagged_dir, "standard/")
 
 def tag_dir(directory_to_tag, output_dir):
     files_to_tag = [f for f in listdir(directory_to_tag) if isfile(join(directory_to_tag, f))]
-    
+    index = 0
     for file in files_to_tag:
-        filepath = join(directory_to_tag, file)
-        with open(filepath, 'r') as f:
-            text = "".join(f.readlines())
-            f.close()
-        tagged = nltk.pos_tag(word_tokenize(text))
-        with open(join(output_dir, file), 'w') as f:
-            f.writelines(str(tagged))
-            f.close()
+        try:
+            filepath = join(directory_to_tag, file)
+            with open(filepath, 'r') as f:
+                text = "".join(f.readlines())
+                f.close()
+            tagged = nltk.pos_tag(word_tokenize(text))
+            with open(join(output_dir, file), 'w') as f:
+                f.writelines(str(tagged))
+                f.close()
+            print(index)
+            index += 1
+        except Exception as e:
+            print(e)
+            print("Error with file: ", file)
+            continue
 
+start = time.time()
 tag_dir(leichte_sprache_dataset, tagged_leicht)
 tag_dir(standard_sprache_dataset, tagged_standard)
+
+end = time.time()
+print("Time: ", end - start)
